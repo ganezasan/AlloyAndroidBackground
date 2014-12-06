2045ba85e9d8b00f943c3bd315a71cb46ea72c15
manager <investi0173gate@yahoo.co.jp>
2014-12-06 21:38:30 +0900

    first commit

---
+++ app/alloy.js
@@ -0,0 +1,42 @@
+// The contents of this file will be executed before any of
+// your view controllers are ever executed, including the index.
+// You have access to all functionality on the `Alloy` namespace.
+//
+// This is a great place to do any initialization for your app
+// or create any global variables/functions that you'd like to
+// make available throughout your app. You can easily make things
+// accessible globally by attaching them to the `Alloy.Globals`
+// object. For example:
+//
+// Alloy.Globals.someGlobalFunction = function(){};
+Alloy.Globals.updateNow = false;
+Alloy.Globals.activeFlg = true;
+Alloy.Globals.activity = null;
+Alloy.Globals.service = null;
+Alloy.Globals.serviceIntent = null;
+Alloy.Globals.lastClosed = Ti.App.Properties.getObject("lastClosed", new Date());   // アプリが最後に閉じられた時間
+Alloy.Globals.serviceOften = 3000;  // サービス起動間隔
+Alloy.Globals.updateOften = 10000;  // 最後にアプリを閉じてから新しいデータを読み込むまでの時間
+Alloy.Globals.autoUpdate = true;
+Alloy.Globals.serviceStatus = false;
+
+/**
+* date型を hh:mi 形式に変換
+*/
+function to_hhmi(date) {
+  var date_obj = new Date(date);
+  var hh = date_obj.getHours();
+  var mi = toDoubleDigits(date_obj.getMinutes());
+  return hh + ':' + mi;
+}
+
+/**
+*  0パディング
+*/
+function toDoubleDigits(num) {
+  num += "";
+  if (num.length === 1) {
+   num = "0" + num;
+  }
+  return num;
+}
---
+++ app/controllers/index.js
@@ -0,0 +1,76 @@
+function doClick(e) {
+    if(Alloy.Globals.serviceStatus){
+      Alloy.Globals.service.stop();
+      Alloy.Globals.serviceStatus = false;
+      $.label.text = "サービスを開始する";
+      $.label.color = "green"
+      alert("サービズを停止しました");
+    }else{
+      Alloy.Globals.service.start();
+      Alloy.Globals.serviceStatus = true;
+      $.label.text = "サービスを停止する";
+      $.label.color = "red"
+      alert("サービズを開始しました");
+    }
+}
+
+$.index.open();
+
+//Activityのライフサイクルに応じてイベントを設定
+$.index.addEventListener("open", function () {
+  Ti.API.info("open");
+  Ti.API.info(Alloy.Globals.service);
+  if(Alloy.Globals.serviceIntent !== null && Ti.Android.isServiceRunning(Alloy.Globals.serviceIntent)){
+    Ti.API.info(Ti.Android.isServiceRunning(Alloy.Globals.serviceIntent));
+    return false;
+  }
+
+  Alloy.Globals.activity = $.index.activity;
+  Alloy.Globals.page = "index";
+  Alloy.Globals.activeFlg = true;
+
+  Alloy.Globals.activity.addEventListener("resume", function(d) {
+    Ti.API.info("************************ Activity was resumed（再開したよ） ************************");
+    Ti.API.info(Alloy.Globals.page);
+
+    Alloy.Globals.activeFlg = true;
+    Ti.API.info("activeFlg: true");
+    Ti.API.info(Alloy.Globals.activeFlg);
+  });
+  Alloy.Globals.activity.addEventListener("pause", function(d) {
+    Ti.API.info("************************ Activity was paused（バックグラウンドにはいったよ） ************************");
+    Ti.API.info(Alloy.Globals.page);
+    Ti.API.info(Alloy.Globals.activeFlg);
+
+    if(Alloy.Globals.page == "index" && Alloy.Globals.activeFlg ){
+      Alloy.Globals.activeFlg = false;
+      // アプリが閉じた時の時間を保存
+      Alloy.Globals.lastClosed = new Date();
+      Ti.API.info("lastClosed: " + to_hhmi(Alloy.Globals.lastClosed));
+    }
+  });
+  Alloy.Globals.activity.addEventListener("destroy", function() {
+    Ti.API.info("Activity was destroied");
+    Ti.API.info(Alloy.Globals.service);
+    Ti.API.info(Alloy.Globals.serviceIntent);
+  });
+  // サービスを起動(Androidのみ)
+  if (OS_ANDROID && Alloy.Globals.service == null) {
+    Ti.API.info("************************ Service Start ************************");
+    var intent = Titanium.Android.createServiceIntent({url: "service.js"});
+    intent.putExtra("interval", Alloy.Globals.serviceOften);
+    var service = Titanium.Android.createService(intent);
+    service.start();
+    Alloy.Globals.serviceStatus = true;
+    Alloy.Globals.service = service;
+    Alloy.Globals.serviceIntent = intent;
+    $.index.addEventListener('android:back', function(){
+      var intent = Ti.Android.createIntent({
+        action: Ti.Android.ACTION_MAIN
+      });
+      intent.addCategory(Ti.Android.CATEGORY_HOME);
+      Ti.Android.currentActivity.startActivity(intent);
+    });
+  }
+});
+
